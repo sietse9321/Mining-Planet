d981da495217fca54e3be6adf54baf28261c48dc
sietse9321 <99656452+sietse9321@users.noreply.github.com>
2024-06-01 22:55:20 +0200

    drill raycast & timer
    Made the drill back to rayccast and added a timer for the drill as long as the same tile and button is hold for (x time).

--- Assets/Scripts/Drill.cs
+++ Assets/Scripts/Drill.cs
@@ -3,18 +3,20 @@ using UnityEngine.Tilemaps;
 
 public class Drill : MonoBehaviour
 {
-    // How destroyed tiles should look.
     public TileBase destroyedTile;
+    //public for (upgrades?)
+    public float drillingTime = 1f;
     [SerializeField] Transform playerTransform;
+    [SerializeField] LayerMask whatLayer;
 
-    bool isDrilling = false;
-    BoxCollider2D boxCollider;
+    float drillTimer = 0;
+    Vector3Int currentTile;
+
     Tilemap tilemap;
 
     void Start()
     {
         tilemap = FindObjectOfType<Tilemap>();
-        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
@@ -24,46 +26,56 @@ public class Drill : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            boxCollider.enabled = true;
-            print("boxcollider enabled");
+            DrillTiles();
         }
         else
         {
-            boxCollider.enabled = false;
+            drillTimer = 0;
         }
     }
 
     /// <summary>
-    /// Handels the rotation for drill to look at mouse
+    /// Handles the rotation for drill to look at mouse
     /// </summary>
     void MoveDrill()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
-
         Vector3 target = new Vector3(mousePos.x, mousePos.y, transform.position.z);
         Vector3 dir = target - transform.position;
-        dir.z = 0;
         dir.Normalize();
-        // Angle of object to mouse pos
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
+        //debug ray
         Debug.DrawRay(transform.position, dir * 20f, Color.red);
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void DrillTiles()
     {
-        Vector3 hitPosition = Vector3.zero;
-
-        foreach (ContactPoint2D hit in collision.contacts)
+        //shoots raycast
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 1f, whatLayer);
+        //if no collider return
+        if (hit.collider == null)
         {
-            hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
-            hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
-            tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+            currentTile = Vector3Int.zero;
+            drillTimer = 0f;
+            return;
+        }
+        Vector3Int tilePosition = tilemap.WorldToCell(hit.point + (Vector2)transform.right * 0.01f);
+        if (tilePosition == currentTile)
+        {
+            if ((drillTimer += Time.deltaTime) >= drillingTime && tilemap.GetTile(tilePosition) != null)
+            {
+                tilemap.SetTile(tilePosition, destroyedTile);
+                Debug.Log("Tile destroyed at: " + tilePosition);
+                drillTimer = 0f;
+            }
+        }
+        else
+        {
+            currentTile = tilePosition;
+            drillTimer = 0f;
         }
     }
-
     void SnapDrillToPlayer()
     {
         transform.position = playerTransform.transform.position;
