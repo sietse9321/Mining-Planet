401718b57785365bd7c82cbf7d578652e20f3e66
sietse9321 <99656452+sietse9321@users.noreply.github.com>
2024-06-07 10:46:30 +0200

    Main Menu
    created main menu, animations for lamp and remove unneeded references.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ContinueGame()
+    {
+        //load game
+    }
+    public void NewGame()
+    {
+
+    }
+    public void Settings()
+    {
+        //turn on settings
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
