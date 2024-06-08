a7796484a7164113c0263789ffab15c520d51118
Rafael Alcazar Paradas <128701588+Rafael-Roc@users.noreply.github.com>
2024-06-08 15:10:18 +0200

    Feat: kleine aanpassing voor item spawnen

--- Assets/Scripts/Drill.cs
+++ Assets/Scripts/Drill.cs
@@ -21,6 +21,7 @@ public class Drill : MonoBehaviour
 
     Tilemap tilemap;
 
+    Item.ItemType oreType;
 
     void SetTileColor()
     {
@@ -77,11 +78,11 @@ public class Drill : MonoBehaviour
                 tilemap.SetTile(tilePosition, destroyedTile);
                 //Debug.Log("Tile destroyed at: " + tilePosition);
                 drillTimer = 0f;
-                ItemWorld.SpawnItemWorld(hit.point + (Vector2)transform.right * 0.01f, new Item { itemType = Item.ItemType.Iron, amount = 1});
+                ItemWorld.SpawnItemWorld(hit.point + (Vector2)transform.right * 0.01f, new Item { itemType = oreType, amount = 1});
                 //GameObject ore = Instantiate(orePrefab);
                 //oreInfo = ore.gameObject.GetComponent<OreInfo>();
                 //ore.transform.position = hit.point + (Vector2)transform.right * 0.01f;
-                //OreGenerator();
+                OreGenerator();
             }
         }
         else
@@ -94,7 +95,67 @@ public class Drill : MonoBehaviour
     {
         int percentage = Random.Range(1, 101);
 
-        if (percentage > 0 && percentage <= 50)
+
+        switch (percentage)
+        {
+            case > 0 and <= 50:
+            oreType = Item.ItemType.Stone;
+            Debug.Log($"<color={"#808080"}>[STONE]</color>");
+                break;
+            case > 50 and <= 60:
+            oreType = Item.ItemType.Copper;
+            Debug.Log($"<color={"#FFA500"}>[COPPER]</color>");
+                break;
+            case > 60 and <= 85:
+            oreType = Item.ItemType.Iron;
+            Debug.Log($"<color={"#FFFFFF"}>[IRON]</color>");
+                break;
+            case > 85 and <= 95:
+            oreType = Item.ItemType.Malachite;
+            Debug.Log($"<color={"#00FFFF"}>[Malachite]</color>");
+                break;
+            case > 95 and <= 100:
+            oreType = Item.ItemType.Titanium;
+            Debug.Log($"<color={"#FFFFFF"}>[TITANIUM]</color>");
+            break;
+            default:
+            Debug.LogWarning("Big oopsie no lootie" + percentage);
+                break;
+        }
+
+        /*if (percentage > 0 && percentage <= 50)
+        {
+            oreType = Item.ItemType.Stone;
+            Debug.Log($"<color={"#808080"}>[STONE]</color>");
+        }
+        else if (percentage > 50 && percentage <= 60)
+        {
+            oreType = Item.ItemType.Copper;
+            Debug.Log($"<color={"#FFA500"}>[COPPER]</color>");
+        }
+        else if (percentage > 60 && percentage <= 85)
+        {
+            oreType = Item.ItemType.Iron;
+            Debug.Log($"<color={"#FFFFFF"}>[IRON]</color>");
+        }
+        else if (percentage > 85 && percentage <= 95)
+        {
+            oreType = Item.ItemType.Malachite;
+            Debug.Log($"<color={"#00FFFF"}>[Malachite]</color>");
+        }
+        else if (percentage > 95 && percentage <= 100)
+        {
+            
+            oreType = Item.ItemType.Titanium;
+            Debug.Log($"<color={"#FFFFFF"}>[TITANIUM]</color>");
+        }
+        else
+        {
+            Debug.LogWarning("Big oopsie no lootie" + percentage);
+        }
+
+
+        /*if (percentage > 0 && percentage <= 50)
         {
             oreInfo.AssignValues("Stone", 1, Color.white);
             Debug.Log($"<color={"#808080"}>[STONE]</color>");
@@ -123,7 +184,7 @@ public class Drill : MonoBehaviour
         else
         {
             Debug.LogWarning("Big oopsie no lootie" + percentage);
-        }
+        }*/
     }
 
     void SnapDrillToPlayer()
