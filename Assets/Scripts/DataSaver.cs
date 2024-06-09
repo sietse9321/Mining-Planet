d19c65419d2aebfc6a650decbf89aef4dcc4490c
sietse9321 <99656452+sietse9321@users.noreply.github.com>
2024-06-09 18:13:14 +0200

    datasaver

--- Assets/Scripts/DataSaver.cs
+++ Assets/Scripts/DataSaver.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -5,15 +7,70 @@ public class DataSaver : MonoBehaviour
 {
     GridPainter gridPainter;
     Tilemap map;
+    PlayerData playerData = new PlayerData();
+    string filePath;
+
+    private void Awake()
+    {
+        filePath = Application.persistentDataPath + "/GameData.json";
+        Debug.Log(filePath);
+        SaveGame();
+    }
+
     public void SaveGame()
     {
         gridPainter = FindObjectOfType<GridPainter>();
         map = gridPainter.minableMap;
+
+        // Collect tile data
+        playerData.tiles = new List<TileData>();
+        foreach (var pos in map.cellBounds.allPositionsWithin)
+        {
+            TileBase tile = map.GetTile(pos);
+            if (tile != null)
+            {
+                playerData.tiles.Add(new TileData
+                {
+                    position = new Vector3Int(pos.x, pos.y),
+                    tileName = tile.name // Assumes that tile names are unique identifiers
+                });
+            }
+        }
+
+        //Transform playerTransform = FindObjectOfType<Player>().transform;
+        //playerData.playerPosition = playerTransform.position;
+
+        // Serialize to JSON and save
+        string jsonData = JsonUtility.ToJson(playerData, true);
+        File.WriteAllText(filePath, jsonData);
     }
+    public void LoadGame()
+    {
+        string jsonData = File.ReadAllText(filePath);
+        playerData = JsonUtility.FromJson<PlayerData>(jsonData);
+
+        map.ClearAllTiles();
+        //foreach tile data in playerdate.tiles
+        TileBase tile;
+        //if tilebase.tileName == "mine"
+        //{
+        //  map.setTile(tileData.Position, tile);
+        //}
+        
+        //set player transform
+    }
+
     [System.Serializable]
-    public struct PlayerData
+    public struct TileData
+    {
+        public Vector3Int position;
+        public string tileName;
+    }
+
+    [System.Serializable]
+    public class PlayerData
     {
         public Vector3 playerPosition;
-        public Vector3Int tilePositionFG;
+        public List<TileData> tiles;
     }
 }
